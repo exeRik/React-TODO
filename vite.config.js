80ffd2ccd38cbd65a79ee789360e4f841cc067a3
Smarika <149926359+exeRik@users.noreply.github.com>
2025-07-24 15:07:38 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/react-todo-list/",
   plugins: [
     react(),
     tailwindcss(), // make sure this is imported!
